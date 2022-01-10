ea999d7f54beb795059c23f177497e7e8784a55e
czc222 <1263291450@qq.com>
2022-01-10 20:04:41 +0800

    重构Chart1

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,61 +2,12 @@ import React, {useEffect, useRef} from 'react';
 import './home.scss';
 import headerBg from '../images/header.png'
 import * as echarts from 'echarts'
+import {Chart1} from '../components/Chart-1';
 const px = (n) => n / 2420 * (window as any).pageWidth;
 
 
 export const Home = () => {
-  const divRef =useRef(null)
-  useEffect(() => {
-    var myChart = echarts.init(divRef.current);
-    myChart.setOption({
-      textStyle: {
-        fontSize: px(12),
-        color: '#79839E'
-      },
-      title: {show: false},
-      legend: {show: false},
-      xAxis: {
-        data: ['兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区', '兰州新区'],
-        axisTick: {show: false},
-        axisLine: {
-          lineStyle: {color: '#083B70'}
-        },
-        axisLabel: {
-          fontSize: px(12),
-          formatter(val) {
-            if (val.length > 2) {
-              const array = val.split('');
-              array.splice(2, 0, '\n');
-              return array.join('');
-            } else {
-              return val;
-            }
-          }
-        },
-      },
-      grid: {
-        left: px(40),
-        right: px(40),
-        bottom: px(40),
-        top: px(40),
-      },
-      yAxis: {
-        splitLine: {show: false},
-        axisLine: {
-          show: true,
-          lineStyle: {color: '#083B70'}
-        },
-        axisLabel: {
-          fontSize: px(12)
-        }
-      },
-      series: [{
-        type: 'bar',
-        data: [10, 20, 36, 41, 15, 26, 37, 18, 29]
-      }]
-    });
-  }, []);
+
 
 
   return (
@@ -64,12 +15,7 @@ export const Home = () => {
       <header  style={{backgroundImage:`URL(${headerBg})`}}/>
       <main>
         <section className="section1">
-          <div className=" bordered 管辖统计">
-            <h2>案发派出所管辖统计</h2>
-            <div ref={divRef} className="chart">
-
-            </div>
-          </div>
+         <Chart1/>
         </section>
         <section className=" bordered section2"></section>
         <section className=" bordered section3"></section>
