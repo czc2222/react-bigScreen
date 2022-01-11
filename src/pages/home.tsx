397192e3aab516bc600bdb695fae3a6519560579
czc222 <1263291450@qq.com>
2022-01-11 14:41:43 +0800

    制作chart4：折线图

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,11 @@
 import React, {useEffect, useRef} from 'react';
 import './home.scss';
-import headerBg from '../images/header.png'
+import headerBg from '../images/header.png';
 import {Chart1} from '../components/Chart-1';
 import {Chart2} from '../components/Chart-2';
 import {Chart3} from '../components/Chart-3';
+import {Chart4} from '../components/Chart-4';
+
 const px = (n) => n / 2420 * (window as any).pageWidth;
 
 
@@ -12,15 +14,16 @@ export const Home = () => {
 
 
   return (
-    <div className='home'>
-      <header  style={{backgroundImage:`URL(${headerBg})`}}/>
+    <div className="home">
+      <header style={{backgroundImage: `URL(${headerBg})`}}/>
       <main>
         <section className="section1">
           <Chart1/>
           <Chart2/>
         </section>
         <section className=" bordered section2">
-        <Chart3/>
+          <Chart3/>
+          <Chart4/>
         </section>
         <section className=" bordered section3"></section>
         <section className=" bordered section4"></section>
